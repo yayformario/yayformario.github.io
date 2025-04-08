9cd37380fb26c83dec38f8bc3878e876d7d68110
Pixiestickz <23437228+Pixiestickz@users.noreply.github.com>
2025-04-08 16:55:30 -0400

    adding other files. Finishing basic layouts for the website

---
+++ main.js
@@ -0,0 +1,21 @@
+//sample blog posts data
+const blogPosts = [
+    {
+        id: 1,
+        title: "title",
+        content: 'javascript is the backbone',
+        author: 'mario huerta',
+        date: '04/08/2025',
+        tags: ['javascript', 'web dev']
+
+    },
+
+    {
+        id: 2,
+        title: 'title2',
+        content: 'content2',
+        author: 'mh',
+        date: '04/08/2024',
+        tags: ['css', 'layout']
+    }
+]
